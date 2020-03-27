f830dcbd5f0baafd65335adf51b074619e098ed2
ivangrod <ivangutrod@gmail.com>
2020-03-27 19:24:39 +0100

    [money-example] Fix compile errors
    * No class Dollar
    * No constructor
    * No method times(int)
    * No field amount

---
+++ tdd/tdd-by-example/money-example/src/main/java/com/ivangrod/wycash/Dollar.java
@@ -0,0 +1,12 @@
+package com.ivangrod.wycash;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+    }
+
+    public void times(int multiplier) {
+    }
+}
---
+++ tdd/tdd-by-example/money-example/src/test/java/com/ivangrod/wycash/WyCashTest.java
@@ -0,0 +1,16 @@
+package com.ivangrod.wycash;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class WyCashTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
