5368ee438cb3bac6e95e64e6060efec740566005
ivangrod <ivangutrod@gmail.com>
2020-03-28 11:54:44 +0100

    [money-example] Add equality test based on Value Object implementation: https://sourcemaking.com/refactoring/change-reference-to-value

--- tdd/tdd-by-example/money-example/src/main/java/com/ivangrod/wycash/Dollar.java
+++ tdd/tdd-by-example/money-example/src/main/java/com/ivangrod/wycash/Dollar.java
@@ -1,5 +1,7 @@
 package com.ivangrod.wycash;
 
+import java.util.Objects;
+
 public class Dollar {
 
     int amount;
@@ -10,5 +12,10 @@ public class Dollar {
 
     public Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return true;
     }
 }
