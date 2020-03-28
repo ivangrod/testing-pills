f07dff36b9905fcfab3aa7a23e939374089afed5
ivangrod <ivangutrod@gmail.com>
2020-03-28 10:06:23 +0100

    [money-example] Remove duplication step 3

--- tdd/tdd-by-example/money-example/src/main/java/com/ivangrod/wycash/Dollar.java
+++ tdd/tdd-by-example/money-example/src/main/java/com/ivangrod/wycash/Dollar.java
@@ -5,9 +5,10 @@ public class Dollar {
     int amount;
 
     public Dollar(int amount) {
+        this.amount = amount;
     }
 
     public void times(int multiplier) {
-        amount = 5 * multiplier;
+        amount = amount * multiplier;
     }
 }
