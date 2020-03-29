7b642653f32091289848a05a6dee6ee076847b73
ivangrod <ivangutrod@gmail.com>
2020-03-29 12:44:09 +0200

    [money-example] Delete subclasses and redundant test with the new code structure

--- tdd/tdd-by-example/money-example/src/main/java/com/ivangrod/wycash/Dollar.java
+++
@@ -1,8 +0,0 @@
-package com.ivangrod.wycash;
-
-public class Dollar extends Money {
-
-  public Dollar(int amount, String currency) {
-    super(amount, currency);
-  }
-}
--- tdd/tdd-by-example/money-example/src/test/java/com/ivangrod/wycash/WyCashTest.java
+++ tdd/tdd-by-example/money-example/src/test/java/com/ivangrod/wycash/WyCashTest.java
@@ -24,14 +24,7 @@ public class WyCashTest {
   public void testEquality() {
     assertTrue(Money.dollar(5).equals(Money.dollar(5)));
     assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-    assertTrue(Money.franc(5).equals(Money.franc(5)));
-    assertFalse(Money.franc(5).equals(Money.franc(6)));
     assertFalse(Money.franc(5).equals(Money.dollar(5)));
-  }
-
-  @Test
-  public void testDifferentClassEquality() {
-    assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
   }
 
   @Test
