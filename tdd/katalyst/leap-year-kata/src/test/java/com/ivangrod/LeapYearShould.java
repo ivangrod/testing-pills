535371c1176b9ee991cc2f08da4b977657452a48
ivangrod <ivangutrod@gmail.com>
2020-04-10 18:47:44 +0200

    [leap-year-kata] Set up Leap Year project

---
+++ tdd/katalyst/leap-year-kata/src/test/java/com/ivangrod/LeapYearShould.java
@@ -0,0 +1,13 @@
+package com.ivangrod;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class LeapYearShould {
+
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
