4f5e4cf7a6fe443c6f4a7fc9eb2e876c4d0f9b69
ivangrod <ivangutrod@gmail.com>
2020-04-11 12:19:50 +0200

    [roman-numeral-kata] Set up Roman Numeral kata

---
+++ tdd/katalyst/roman-numeral-kata/src/main/java/com/ivangrod/RomanNumeralGenerator.java
@@ -0,0 +1,13 @@
+package com.ivangrod;
+
+/**
+ * Hello world!
+ *
+ */
+public class RomanNumeralGenerator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tdd/katalyst/roman-numeral-kata/src/test/java/com/ivangrod/RomanNumeralGeneratorShould.java
@@ -0,0 +1,3 @@
+package com.ivangrod;
+
+public class RomanNumeralGeneratorShould {}
