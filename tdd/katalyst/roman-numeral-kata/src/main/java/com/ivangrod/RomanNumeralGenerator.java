bc572f72dce543161ed349139fb4187c7c514b18
ivangrod <ivangutrod@gmail.com>
2020-04-11 13:11:31 +0200

    [roman-numeral-kata] Implement recursion for 1 to 4 arabic numbers

--- tdd/katalyst/roman-numeral-kata/src/main/java/com/ivangrod/RomanNumeralGenerator.java
+++ tdd/katalyst/roman-numeral-kata/src/main/java/com/ivangrod/RomanNumeralGenerator.java
@@ -1,13 +1,38 @@
 package com.ivangrod;
 
-/**
- * Hello world!
- *
- */
-public class RomanNumeralGenerator
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanNumeralGenerator {
+
+  private static final Map<Integer, String> oneLiteralNumeral = new HashMap<>();
+
+  static {
+    oneLiteralNumeral.put(1, "I");
+    oneLiteralNumeral.put(5, "V");
+    oneLiteralNumeral.put(10, "X");
+    oneLiteralNumeral.put(50, "L");
+    oneLiteralNumeral.put(100, "C");
+    oneLiteralNumeral.put(500, "D");
+    oneLiteralNumeral.put(1000, "M");
+  }
+
+  public String convert(int arabicNumber) {
+
+    if (arabicNumber == 2 || arabicNumber == 3 || arabicNumber == 4) {
+      return convert_1_to_4_roman_numeral(arabicNumber);
     }
+
+    return oneLiteralNumeral.get(arabicNumber);
+  }
+
+  private String convert_1_to_4_roman_numeral(int arabicNumberFrom1To4) {
+
+    StringBuilder strBuilder = new StringBuilder("");
+    while (arabicNumberFrom1To4 >= 1) {
+      strBuilder.append(convert(1));
+      arabicNumberFrom1To4 -= 1;
+    }
+    return strBuilder.toString();
+  }
 }
--- tdd/katalyst/roman-numeral-kata/src/test/java/com/ivangrod/RomanNumeralGeneratorShould.java
+++ tdd/katalyst/roman-numeral-kata/src/test/java/com/ivangrod/RomanNumeralGeneratorShould.java
@@ -1,3 +1,63 @@
 package com.ivangrod;
 
-public class RomanNumeralGeneratorShould {}
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RomanNumeralGeneratorShould {
+
+  @Test
+  public void convert_1_arabic_number_to_I_roman_numeral() {
+    assertThat("I", equalTo(romanNumeral(1)));
+  }
+
+  @Test
+  public void convert_2_arabic_number_to_II_roman_numeral() {
+    assertThat("II", equalTo(romanNumeral(2)));
+  }
+
+  @Test
+  public void convert_3_arabic_number_to_III_roman_numeral() {
+    assertThat("III", equalTo(romanNumeral(3)));
+  }
+
+  @Test
+  public void convert_4_arabic_number_to_IIII_roman_numeral() {
+    assertThat("IIII", equalTo(romanNumeral(4)));
+  }
+
+  @Test
+  public void convert_5_arabic_number_to_V_roman_numeral() {
+    assertThat("V", equalTo(romanNumeral(5)));
+  }
+
+  @Test
+  public void convert_10_arabic_number_to_X_roman_numeral() {
+    assertThat("X", equalTo(romanNumeral(10)));
+  }
+
+  @Test
+  public void convert_50_arabic_number_to_L_roman_numeral() {
+    assertThat("L", equalTo(romanNumeral(50)));
+  }
+
+  @Test
+  public void convert_100_arabic_number_to_C_roman_numeral() {
+    assertThat("C", equalTo(romanNumeral(100)));
+  }
+
+  @Test
+  public void convert_500_arabic_number_to_D_roman_numeral() {
+    assertThat("D", equalTo(romanNumeral(500)));
+  }
+
+  @Test
+  public void convert_1000_arabic_number_to_M_roman_numeral() {
+    assertThat("M", equalTo(romanNumeral(1000)));
+  }
+
+  private String romanNumeral(int arabicNumber) {
+    return new RomanNumeralGenerator().convert(arabicNumber);
+  }
+}
